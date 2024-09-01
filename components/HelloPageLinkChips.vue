7b45418f91bfae078ab598774ed75613c21fee44
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-09-01 09:47:37 +0800

    feat: Links

--- components/HelloPageLinkChips.vue
+++ components/HelloPageLinkChips.vue
@@ -73,5 +73,21 @@
         style="vertical-align: -0.125rem"
       /> Matrix
     </NuxtLink>
+    <NuxtLink
+      href="/links"
+      rounded="2"
+      px="2"
+      py="1"
+      ring="pink-500 focus:2"
+      bg="gray-200 hover:pink-200 dark:pink-900 dark:hover:gray-900"
+      un-text="hover:pink-700"
+      transition
+      duration="200"
+    >
+      <Icon
+        name="ph:link-duotone"
+        style="vertical-align: -0.125rem"
+      /> Links
+    </NuxtLink>
   </div>
 </template>
---
+++ content/links.md
@@ -0,0 +1,16 @@
+---
+title: "Links"
+layout: page
+---
+
+# Links
+
+**THIS IS RATHER PRIMITIVE.**
+
+* [惠狐之书](https://blog.megumifox.com/public/)
+* [约伊兹的萌狼乡手札](https://blog.yoitsu.moe)
+* [OriginCode 札記](https://blog.origincode.me)
+* [Vifly 的博客](https://viflythink.com)
+* [玖只的尾巴](https://blog.yurzi.net)
+* [Kimiblock's Blog](https://blog.kimiblock.top)
+* [Guanran928 的博客。](https://blog.ny4.dev)
